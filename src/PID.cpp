c6ebe30fa553a85db1864c1cdc1c70500c875d14
Uwe Ehmann <uwe.ehmann@bmw.de>
2019-11-19 20:38:53 +0100

    implementation of the thing

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,39 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
-
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+    Kp = Kp_;
+    Ki = Ki_;
+    Kd = Kd_;
 
+    p_error = 0.0;
+    d_error = 0.0;
+    i_error = 0.0;
+
+    prev_cte = 0.0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
 
+    // Proportional error: this term is proportional to the cte.
+    // Effect: The stronger we are off-located from the center of the road the more we steer towards it.
+    // Using this error only leads to oscillations.
+    p_error = cte;
+
+    // Differential error: this term is driven by the difference in cte (d(cte)/dt) with respect to previous steps.
+    // Effect: It reduces/damps the oscillations induced by p_error.
+    d_error = cte - prev_cte;
+
+    // Integral error: this term represents the sum of all ctes of previous steps.
+    // Effect: It accounts for constant steering biases.
+    i_error += cte;
+
+    prev_cte = cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    return p_error * Kp + d_error * Kd + i_error * Ki;
 }
